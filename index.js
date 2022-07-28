8d595aa995bdeb63aaddb5a9efbb31f7ef07361b
NKDuy <kn145660@gmail.com>
2022-07-28 15:12:40 +0700

    6.1.0

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
+// https://github.com/benmosher/eslint-plugin-import/issues/1396
+const resolver = {
+  [require.resolve('eslint-import-resolver-node')]: {},
+}
+
+// in case the project does not depend on @kdujs/cli-service
+try {
+  resolver[require.resolve('eslint-import-resolver-webpack')] = {
+    config: require.resolve('@kdujs/cli-service/webpack.config.js'),
+  }
+} catch (e) {}
+
 module.exports = {
   extends: [
     require.resolve('eslint-config-standard')
-  ]
+  ],
+  settings: {
+    'import/resolver': resolver,
+    'import/extensions': [
+      '.js',
+      '.jsx',
+      '.mjs',
+      '.ts',
+      '.tsx',
+    ],
+  },
 }
